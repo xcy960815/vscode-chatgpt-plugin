1236b351e87730f0eb73f1e216e02166ce40f769
Opera <opera@OperadeMacBook-Pro.local>
2023-04-13 17:06:00 +0800

    feat: 添加格式化

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 单行最大长度
+  printWidth: 60,
+  // 设置编辑器每一个水平缩进的空格数
+  tabWidth: 2,
+  // 在句尾添加分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 在任何可能的多行中输入尾逗号。
+  trailingComma: 'none',
+  // 在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
+  bracketSpacing: true,
+  // 在多行JSX元素最后一行的末尾添加 > 而使 > 单独一行（不适用于自闭和元素）
+  jsxBracketSameLinte: false,
+  // 为单行箭头函数的参数添加圆括号。
+  alwaysParens: 'always',
+};
